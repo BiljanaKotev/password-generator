8e47391e97f289f7fe0130b6186c92c3faed7905
biljana_kotev <biljanakotev@gmail.com>
2023-01-17 18:57:11 +0100

    updated function

--- src/index.js
+++ src/index.js
@@ -1,101 +1,23 @@
 "use strict";
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
-const inputField = document.getElementById("input");
+
 const passwordBtn = document.querySelector("#password-btn");
-const passwordLength = 12;
 
 passwordBtn.addEventListener("click", generatePassword);
 
-function generatePassword() {}
+function generatePassword() {
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const passwordLength = 12;
+  let password1 = "";
+  let password2 = "";
+
+  for (var i = 0; i <= passwordLength; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password1 += chars.substring(randomNumber, randomNumber + 1);
+    const randomNumber2 = Math.floor(Math.random() * chars.length);
+    password2 += chars.substring(randomNumber2, randomNumber2 + 1);
+  }
+
+  document.getElementById("password-one").value = password1;
+  document.getElementById("password-two").value = password2;
+}
