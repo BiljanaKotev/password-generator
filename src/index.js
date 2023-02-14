b464d157656dc12e20ceedb8f5401e0701047ece
biljana_kotev <biljanakotev@gmail.com>
2023-02-14 09:00:11 +0100

    deleted comments

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 "use strict";
 
-// const passwordBtn = document.querySelector("#password-btn");
-// passwordBtn.addEventListener("click", generatePassword);
-
-// function generatePassword() {
-//   const chars =
-//     "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-//   const passwordLength = 12;
-//   let password1 = "";
-//   let password2 = "";
-
-//   for (var i = 0; i <= passwordLength; i++) {
-//     const randomNumber = Math.floor(Math.random() * chars.length);
-//     password1 += chars.substring(randomNumber, randomNumber + 1);
-//     const randomNumber2 = Math.floor(Math.random() * chars.length);
-//     password2 += chars.substring(randomNumber2, randomNumber2 + 1);
-//   }
-
-//   document.getElementById("password-one").value = password1;
-//   document.getElementById("password-two").value = password2;
-// }
-
-// Variables
-// Variables
 let characters = [
   "A",
   "B",
